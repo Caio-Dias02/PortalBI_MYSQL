a0e01b5eb02b374c99a445bf539fb0c39560d272
Caio Dias <82725503+Caio-Dias02@users.noreply.github.com>
2023-07-07 10:14:39 -0300

    Update database.php

--- database.php
+++ database.php
@@ -1,16 +1,16 @@
 <?php
 
-$host = "localhost";
-$user = "root";
-$senha = "";
-$database = "portalweb";
+$host = "sql107.infinityfree.com";
+$user = "epiz_34340527";
+$senha = "IylzuCyYi6ea";
+$database = "epiz_34340527_portalweb";
 
 $mysqli = new mysqli($host, $user, $senha, $database);
 
-define("DB_SERVER", "localhost");
-define("DB_USER", "root");
-define("DB_PASSWORD", "");
-define("DB_DATABASE", "portalweb");
+define("DB_SERVER", "sql107.infinityfree.com");
+define("DB_USER", "epiz_34340527");
+define("DB_PASSWORD", "IylzuCyYi6ea");
+define("DB_DATABASE", "epiz_34340527_portalweb");
 
 $connect = mysqli_connect(DB_SERVER , DB_USER, DB_PASSWORD, DB_DATABASE);
 
